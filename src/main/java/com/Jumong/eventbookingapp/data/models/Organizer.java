19e44f5d8ef6fd6a4c74519efd83492ce4d914b4
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-07-03 11:12:00 +0100

    first commit

--- src/main/java/com/Jumong/eventbookingapp/data/models/Organizer.java
+++ src/main/java/com/Jumong/eventbookingapp/data/models/Organizer.java
@@ -1,2 +1,22 @@
-package com.Jumong.eventbookingapp.data.models;public class Organizer {
+package com.Jumong.eventbookingapp.data.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "organizers")
+public class Organizer {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
 }
